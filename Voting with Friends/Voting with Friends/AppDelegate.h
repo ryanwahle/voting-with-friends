6ee2667d4bd3829795456f3687cec918924a3ef4
Ryan Wahle <ryanwahle@fullsail.edu>
2015-03-18 10:54:16 -0700

    initial commit

---
+++ Voting with Friends/Voting with Friends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Voting with Friends
+//
+//  Created by Ryan Wahle on 3/18/15.
+//  Copyright (c) 2015 Ryan Wahle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
