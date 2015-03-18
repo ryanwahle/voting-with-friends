4ee95b5b7ccd2bf02f26655cd3e4f0d4a1fcbc2d
Ryan Wahle <ryanwahle@fullsail.edu>
2015-03-18 14:31:30 -0700

    added vote for poll user interface

---
+++ Voting with Friends/Voting with Friends/PollsVC.h
@@ -0,0 +1,13 @@
+//
+//  PollsVC.h
+//  Voting with Friends
+//
+//  Created by Ryan Wahle on 3/18/15.
+//  Copyright (c) 2015 Ryan Wahle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PollsVC : UITableViewController
+
+@end
