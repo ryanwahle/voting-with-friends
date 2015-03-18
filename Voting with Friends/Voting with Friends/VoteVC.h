b20f8182456ca632024512c67d2b70dc1290c076
Ryan Wahle <ryanwahle@fullsail.edu>
2015-03-18 16:50:44 -0700

    converted vote user interface to use tables.

---
+++ Voting with Friends/Voting with Friends/VoteVC.h
@@ -0,0 +1,13 @@
+//
+//  VoteVC.h
+//  Voting with Friends
+//
+//  Created by Ryan Wahle on 3/18/15.
+//  Copyright (c) 2015 Ryan Wahle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VoteVC : UITableViewController
+
+@end
---
+++ Voting with Friends/Voting with Friends/VoteVC.m
@@ -0,0 +1,140 @@
+//
+//  VoteVC.m
+//  Voting with Friends
+//
+//  Created by Ryan Wahle on 3/18/15.
+//  Copyright (c) 2015 Ryan Wahle. All rights reserved.
+//
+
+#import "VoteVC.h"
+
+@interface VoteVC ()
+
+@end
+
+@implementation VoteVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return 1;
+    } else if (section == 1) {
+        return 4;
+    } else if (section == 2) {
+        return 3;
+    }
+    
+    
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = nil;
+    
+    if (indexPath.section == 0) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cellQuestion" forIndexPath:indexPath];
+    } else if (indexPath.section == 1) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cellAnswer" forIndexPath:indexPath];
+    } else if (indexPath.section == 2) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cellActivity" forIndexPath:indexPath];
+    }
+    
+    return cell;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UITableViewCell *sectionCell = nil;
+    
+    
+    if (section == 0) {
+        sectionCell = [tableView dequeueReusableCellWithIdentifier:@"headerQuestion"];
+    } else if (section == 1) {
+        //sectionCell = [tableView dequeueReusableCellWithIdentifier:@"headerQuestion"];
+    } else if (section == 2) {
+        sectionCell = [tableView dequeueReusableCellWithIdentifier:@"headerActivity"];
+    }
+    
+    return sectionCell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 50.0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 0) {
+        return 88;
+    } else if (indexPath.section == 1) {
+        return 44;
+    } else if (indexPath.section == 2) {
+        return 44;
+    }
+    
+    return 44;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
